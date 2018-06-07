625ba1317a890804c9caac06384264697ff0e3a2
Tim Preston <tmpreston@users.noreply.github.com>
2018-06-07 22:38:33 +1000

    Format by .editorconfig Basic feedback via console while running

--- src/CatalogOperation.cs
+++ src/CatalogOperation.cs
@@ -1,24 +1,38 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace duplicatus
 {
-	public class CatalogOperation
-	{
-		private string _basePath;
-		public CatalogOperation(string basePath)
-		{
-			_basePath=basePath;
-		}
+    public class CatalogOperation
+    {
+        private string _basePath;
+        private Action<FileInformation> _operation;
 
-		public IEnumerable<FileInformation> Run()
-		{
-			var directory = new DirectoryInfo(_basePath);
-			foreach(var file in directory.EnumerateFiles("*.*", SearchOption.AllDirectories))
-			{
-				yield return FileInformation.FromFile(file);
-			}
-		}
-	}
+        public CatalogOperation(string basePath, Action<FileInformation> operation = null)
+        {
+            _basePath = basePath;
+            if (operation == null)
+            {
+                operation = (file) => { };
+            }
+            _operation = operation;
+        }
+
+        public FileInformation[] Run()
+        {
+            var directory = new DirectoryInfo(_basePath);
+            var fileInfos = from file in directory.EnumerateFiles("*.*", SearchOption.AllDirectories)
+                            select file;
+            var convertedInfos = from info in fileInfos.AsParallel()
+                                 select FileInformation.FromFile(info);
+            foreach (var fileInformation in convertedInfos)
+            {
+                _operation(fileInformation);
+            }
+            return convertedInfos.ToArray();
+        }
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks; 
+using System.Threading.Tasks;
 
 namespace duplicatus
 {
@@ -9,31 +9,47 @@ namespace duplicatus
     {
         static async Task<int> Main(string[] args)
         {
-            if(args.Length < 2)
+            if (args.Length < 2)
             {
                 Console.WriteLine("Requires two args.  [PathToCheck] [FileToSave]");
                 return -1;
             }
-            var pathToCheck=args[0];
+            var pathToCheck = args[0];
 
-            Console.WriteLine($"Checking path: '{args[0]}' and saving catalog to '{args[1]}'.  Press Y to continue.");
-            if(Console.ReadKey().KeyChar != 'y')
-            {
-                return -1;
-            }
+            Console.WriteLine($"Checking path: '{args[0]}' and saving catalog to '{args[1]}'.");
             Console.WriteLine("\nStarting catalog...");
             var sw = new System.Diagnostics.Stopwatch();
             sw.Start();
-            var co = new CatalogOperation(args[0]);
-            var files=co.Run().ToList();
+            int filesProcessed = 0;
+            string lastFile = null;
+            bool running = true;
+            Task.Run(() =>
+            {
+                do
+                {
+                    if(!running) break;
+                    Console.Clear();
+                    Console.WriteLine($"Files processed: {filesProcessed}");
+                    Console.WriteLine($"Current file: {lastFile}");
+                    System.Threading.Thread.Sleep(500);
+                } while (running);
+            });
+            Action<FileInformation> trackProgress = (info) =>
+            {
+                System.Threading.Interlocked.Increment(ref filesProcessed);
+                lastFile = info.AbsolutePath;
+            };
+            var co = new CatalogOperation(args[0], trackProgress);
+            var files = co.Run().ToList();
+            running=false;
             Console.WriteLine("Time to index: {0} files.  {1}ms", files.Count, sw.ElapsedMilliseconds);
-            var duplicates = files.GroupBy(zz=>zz.MD5Hash, new ByteArrayComparer()).Where(zz=>zz.Count() > 1).ToList();
+            var duplicates = files.GroupBy(zz => zz.MD5Hash, new ByteArrayComparer()).Where(zz => zz.Count() > 1).ToList();
             Console.WriteLine($"{duplicates.Count()} duplicates.");
-            foreach(var f in duplicates)
+            foreach (var f in duplicates)
             {
                 var md5String = BitConverter.ToString(f.First().MD5Hash).Replace("-", "");
                 Console.WriteLine($"{md5String} - {f.Count()} matching files.");
-                Console.WriteLine("\t{0}", string.Join("\r\n\t", f.Select(zz=>zz.AbsolutePath)));
+                Console.WriteLine("\t{0}", string.Join("\r\n\t", f.Select(zz => zz.AbsolutePath)));
             }
             return 0;
         }
